64e80f1f9446b17bd178d9e507e30d702f9e25f2
trived76 <maharshi.trivedi@mail.utoronto.ca>
2019-04-16 16:18:48 -0400

    Python communicating with Arduino Pyserial

---
+++ Arduino_with_Python_Pyserial/sketch_apr12a.ino
@@ -0,0 +1,45 @@
+#define sensor A0
+
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  float volts = analogRead(sensor)*0.0048828125;
+  int distance = 13*pow(volts,-1);
+  
+  if (distance <20)
+  {
+    Serial.println(distance);
+    delay(10);
+    
+    if(Serial.available())
+   {
+     int category = Serial.read();
+     if (category == 0)
+     {
+       digitalWrite(2,HIGH);
+       digitalWrite(6,LOW);
+       digitalWrite(10,LOW);
+     }
+     else if (category == 1)
+     {
+       digitalWrite(2,LOW);
+       digitalWrite(6,HIGH);
+       digitalWrite(10,LOW);
+     }
+     else if (category == 2)
+     {
+       digitalWrite(2,LOW);
+       digitalWrite(6,LOW);
+       digitalWrite(10,HIGH);
+     }
+     delay(500);
+  }
+}
+}
